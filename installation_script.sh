698f4d342f0b889d23a17f3c857b9cbe9bafddf5
Ryosuke Minami <ryosukem13@gmail.com>
2018-10-01 01:19:36 +0100

    Fixed installation script

--- installation_script.sh
+++ installation_script.sh
@@ -2,12 +2,12 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install geometry theme
-git clone https://github.com/geometry-zsh/geometry.git $HOME/.oh-my-zsh/custom/themes
+git clone https://github.com/geometry-zsh/geometry.git $HOME/.oh-my-zsh/custom/themes/geometry
 ln -s $HOME/.oh-my-zsh/custom/themes/geometry/geometry.zsh-theme $HOME/.oh-my-zsh/custom/themes/geometry.zsh-theme
 
 # Replace rc files
 rm -f $HOME/.bashrc $HOME/.bash_profile $HOME/.zshrc $HOME/.zshrc.pre-oh-my-zsh
-ln -s .zshrc $HOME/.zshrc
-ln -s .bashrc $HOME/.bashrc
-ln -s .bash_profile $HOME/.bash_profile
+ln -s $PWD/.zshrc $HOME/.zshrc
+ln -s $PWD/.bashrc $HOME/.bashrc
+ln -s $PWD/.bash_profile $HOME/.bash_profile
 
